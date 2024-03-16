aee1f85e43942a70b9bc746b1db6f6224ae4edd7
Chris Claude <christ.tchambila@gmail.com>
2024-03-16 23:33:38 +0100

    initial commit

---
+++ src/features/counter/counterSlice.ts
@@ -0,0 +1,17 @@
+// DUCKS pattern
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface CounterState {
+  value: number;
+}
+
+const initialState: CounterState = {
+  value: 0,
+}
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+  }
+});
